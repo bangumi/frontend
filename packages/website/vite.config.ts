429c828a6858bfd9a12c32978976846b168108b7
Qingyu Deng <i@ayase-lab.com>
2022-01-06 20:11:51 +0800

    ci: fix vercel preview (#7)
    * ci: add ci action to make sure project can build
    
    * fixup
    
    * build: set library name change case to CapitalCase

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
       libs: [
         {
           libraryName: '@bangumi/design',
+          libraryNameChangeCase: 'capitalCase',
           ensureStyleFile: true,
           resolveStyle: (name: string) => `@bangumi/design/components/${name}/style/index.tsx`
         }
