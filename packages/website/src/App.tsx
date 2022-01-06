8de58b6734b39314df3142ce56629a8cbc1e9cfe
cokemine <aptx4561@gmail.com>
2022-01-06 13:57:08 +0800

    refactor: use css modules

--- packages/website/src/App.tsx
+++ packages/website/src/App.tsx
@@ -1,17 +1,17 @@
 import React, { useState, FC } from 'react'
 import { Button } from '@bangumi/design'
-import './App.css'
+import style from './App.module.css'
 
 const App: FC = () => {
   const [count, setCount] = useState(0)
   return (
-    <div className="chii-main">
-      <div className="chii-count">
+    <div className={style.main}>
+      <div className={style.count}>
         {count}
       </div>
       <Button
         onClick={() => setCount(count => count + 1)}
-        className="chii-button"
+        className={style.button}
       >
         Increase
       </Button>
