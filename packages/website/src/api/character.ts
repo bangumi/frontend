814dd38a4a72fe5d8b59cb79e5ead0a1df411989
shadowdreamer <uzumaki@yeah.net>
2022-01-15 18:00:03 +0800

    style: remove api map

--- packages/website/src/api/character.ts
+++ packages/website/src/api/character.ts
@@ -2,10 +2,6 @@ import { request } from './request'
 import { CharacterDetail } from '../types/common'
 import { AxiosPromise } from 'axios'
 
-const api = {
-  getCharacterDetail: '/v0/characters/'
-}
-
 type CharacterId = string;
 
 export const getCharacterDetail = (characterId:CharacterId):AxiosPromise<CharacterDetail> => {
