e53a4c02d89ac467f187e4b342c75813be0fe3e9
Qingyu Deng <i@ayase-lab.com>
2022-01-17 22:33:14 +0800

    fix: import style automatically when serving storybook
    Co-authored-by: 神楽坂みずき <45122329+cokemine@users.noreply.github.com>

--- packages/design/.storybook/main.js
+++ packages/design/.storybook/main.js
@@ -14,6 +14,14 @@ module.exports = {
     // workaround for vite build
     // Refs: https://github.com/eirslett/storybook-builder-vite/issues/55#issuecomment-871800293
     viteConfig.root = dirname(require.resolve("storybook-builder-vite"))
+    viteConfig.plugins.push({
+      transform(source, id) {
+        if (id.endsWith('.stories.tsx')) {
+          return `${source}
+          import './style'`
+        }
+      }
+    })
     return viteConfig
   },
   core: {
