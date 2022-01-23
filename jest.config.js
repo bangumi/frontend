97016c6764481b07cb900ccf7c862c028436ebc4
Qingyu Deng <i@ayase-lab.com>
2022-01-23 18:05:15 +0800

    test: ignore coverage collection from mock file

--- jest.config.js
+++ jest.config.js
@@ -26,9 +26,10 @@ module.exports = {
   // coverageDirectory: undefined,
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    'package/website/src/mocks/'
+  ],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
