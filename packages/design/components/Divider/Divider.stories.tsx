90b28f949e882d4c4338b70af443d8457b265761
cijiugechu <cijiugechu@gmail.com>
2022-02-13 00:30:30 +0800

    fix(storybook): add Wrapper

--- packages/design/components/Divider/Divider.stories.tsx
+++ packages/design/components/Divider/Divider.stories.tsx
@@ -4,10 +4,34 @@ import Divider from '.'
 
 export default {
   title: 'Grid/Divider',
-  component: Divider
+  component: Divider,
+  decorators: [
+    story => <div style={{ width: '15vw', height: '15vh' }}>{story()}</div>
+  ]
 } as ComponentMeta<typeof Divider>
 
-const Template: ComponentStory<typeof Divider> = (args) => <Divider {...args} />
+const Template: ComponentStory<typeof Divider> = (args) => {
+  const isListItem = args.isListItem
+  const orientation = args.orientation
+
+  if (isListItem) {
+    return <ul style={{
+      listStyle: 'none',
+      display: 'flex',
+      flexDirection: orientation === 'vertical' ? 'row' : 'column'
+    }}>
+      <li>想看</li>
+      <Divider {...args} />
+      <li>看过</li>
+    </ul>
+  } else {
+    return <div style={{ display: orientation === 'vertical' ? 'flex' : undefined }}>
+      <span>标题</span>
+      <Divider {...args} />
+      <span>文本</span>
+    </div>
+  }
+}
 
 export const Horizontal = Template.bind({})
 Horizontal.args = {
