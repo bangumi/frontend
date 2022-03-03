1657bc02b9fafba1413f52e3214254970df870c6
cokemine <aptx4561@gmail.com>
2022-03-03 11:29:04 +0800

    refactor

--- packages/design/components/Menu/MenuItem.tsx
+++ packages/design/components/Menu/MenuItem.tsx
@@ -5,17 +5,19 @@ import { useMenuContext } from '.'
 export interface MenuItemProps {
   /* MenuItem 的唯一标识 */
   id: string,
+  /* 自定义类名，大多数情况下应该尽可能为父组件设置样式 */
+  className?: string,
   /* 覆盖父组件 Menu 的默认 Click 事件, 但大多数情况下应该尽可能使用父组件的 Click 事件 */
   onClick?: MouseEventHandler,
 }
 
-const MenuItem: FC<MenuItemProps> = ({ children, onClick, id }) => {
+const MenuItem: FC<MenuItemProps> = ({ children, onClick, id, className }) => {
   const { onClick: onClickEmit, activeKey } = useMenuContext()
   return (
     <li
       className={classnames('bgm-menu-item', {
         'bgm-menu-item--active': id === activeKey
-      })}
+      }, className)}
       onClick={onClick || (e => onClickEmit?.(id, e))}
     >
       {children}
