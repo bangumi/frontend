2633e789a2bc276a53d534842b831c3b6f5a47b0
Qingyu Deng <i@ayase-lab.com>
2022-03-04 23:54:05 +0800

    fix: comply with convention

--- packages/design/components/Rate/index.tsx
+++ packages/design/components/Rate/index.tsx
@@ -15,9 +15,13 @@ const Rate: React.FC<RateProps> = (props) => {
   const numEmptyStars = 5 - numHalfStars - numFiledStars
 
   return <div className="bgm-rate">
-    { Array.from({ length: numFiledStars }).map((_, i) => <FilledStar className="star" key={`filled-${i}`} data-testid="filled"/>) }
-    { numHalfStars ? <HalfStar className="star" data-testid="half"/> : null}
-    { Array.from({ length: numEmptyStars }).map((_, i) => <EmptyStar className="star" key={`empty-${i}`} data-testid="empty"/>) }
+    { Array.from({ length: numFiledStars }).map(
+      (_, i) => <FilledStar className="bgm-rate__star" key={`filled-${i}`} data-testid="filled"/>
+    ) }
+    { numHalfStars ? <HalfStar className="bgm-rate__star" data-testid="half"/> : null}
+    { Array.from({ length: numEmptyStars }).map(
+      (_, i) => <EmptyStar className="bgm-rate__star" key={`empty-${i}`} data-testid="empty"/>
+    ) }
   </div>
 }
 
