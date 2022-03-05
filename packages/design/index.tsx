4a9323b754600abe050e8a01980a44ab64af1e34
cijiugechu <cijiugechu@gmail.com>
2022-03-05 20:06:47 +0800

    fix: export Divider component

--- packages/design/index.tsx
+++ packages/design/index.tsx
@@ -1,6 +1,7 @@
 export { default as Button } from './components/Button'
 export { default as Typography } from './components/Typography'
 export { default as Tab } from './components/Tab'
+export { default as Divider } from './components/Divider'
 export { default as Menu, MenuItem } from './components/Menu'
 
 export type { MenuProps, MenuItemProps } from './components/Menu'
