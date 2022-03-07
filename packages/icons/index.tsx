d2335423c00dabb12bcd663918c65b8846ce9f51
cokemine <aptx4561@gmail.com>
2022-03-07 11:24:50 +0800

    refactor: move icons to `@bangumi/icons`

---
+++ packages/icons/index.tsx
@@ -0,0 +1,8 @@
+export { ReactComponent as FilledStar } from './assets/filled-star.svg'
+export { ReactComponent as HalfStar } from './assets/half-star.svg'
+export { ReactComponent as EmptyStar } from './assets/empty-star.svg'
+
+export { ReactComponent as Search } from './assets/search.svg'
+export { ReactComponent as Setting } from './assets/setting.svg'
+export { ReactComponent as Notification } from './assets/notification.svg'
+export { ReactComponent as Delete } from './assets/delete.svg'
