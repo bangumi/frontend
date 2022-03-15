ccbde519c37769501c1beef3755a379d249655b2
神楽坂みずき <45122329+cokemine@users.noreply.github.com>
2022-03-15 21:37:27 +0800

    feat(design): implement avatar component (#51)
    Co-authored-by: Qingyu Deng <i@ayase-lab.com>

---
+++ packages/design/components/Avatar/index.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from 'react'
+import classNames from 'classnames'
+
+export interface AvatarProps {
+  /* 头像大小 */
+  size?: 'small' | 'medium' | 'large'
+  /* 头像的 URL */
+  src: string
+  /* 替代文本 */
+  alt?: string
+  /* 自定义最外层类名 */
+  wrapperClass?: string
+  /* 自定义最外层样式 */
+  wrapperStyle?: React.CSSProperties
+}
+
+const Avatar: FC<AvatarProps> = ({
+  size = 'small',
+  src,
+  alt,
+  wrapperClass,
+  wrapperStyle
+}) => {
+  return (
+    <div className={classNames('bgm-avatar', `bgm-avatar--${size}`, wrapperClass)} style={wrapperStyle}>
+      <img src={src} alt={alt} />
+    </div>
+  )
+}
+export default Avatar
