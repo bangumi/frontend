2c9dc035fa6994d118ea65c23ebd2ca28c862fd4
cokemine <aptx4561@gmail.com>
2022-03-18 15:17:39 +0800

    refactor

--- packages/utils/wiki/__test__/parser.test.ts
+++ packages/utils/wiki/__test__/parser.test.ts
@@ -9,16 +9,19 @@ describe('Wiki syntax parser', () => {
   files.forEach(file => {
     const prefix = file.split('.')[0]
     const suffix = file.split('.')[1]
-    if (suffix === 'yaml') {
+    if (suffix !== 'wiki') {
       return
     }
     it(`should parse ${prefix}`, () => {
       const testFilePath = path.resolve(__dirname, './wiki-syntax-spec/tests/valid', file)
       const expectedFilePath = path.resolve(__dirname, './wiki-syntax-spec/tests/valid', `${prefix}.yaml`)
+
       const testContent = fs.readFileSync(testFilePath, 'utf8')
       const expectedContent = fs.readFileSync(expectedFilePath, 'utf8')
+
       const result = parse(testContent)
       const expected = yaml.load(expectedContent)
+
       expect(result).toEqual(expected)
     })
   })
