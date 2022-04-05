a62c4fb87a95c22f752057acd2572dd8ad64c18d
Qingyu Deng <i@ayase-lab.com>
2022-04-05 13:22:04 +0800

    refactor: add GlobalLayout

--- packages/website/src/App.tsx
+++ packages/website/src/App.tsx
@@ -1,23 +1,20 @@
 import React, { FC } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
-import Footer from './components/Footer'
-import Header from './components/Header'
+import GlobalLayout from './components/GlobalLayout'
 import PageRoutes from './components/PageRoutes'
 import Home from './pages/Home'
 import Subject from './pages/Subject'
 
 const App: FC = () => {
   return (
-    <>
-      <Header />
+    <GlobalLayout>
       <BrowserRouter>
         <PageRoutes>
           <Route path="subject/*" element={<Subject />} />
           <Route index element={<Home />} />
         </PageRoutes>
       </BrowserRouter>
-      <Footer />
-    </>
+    </GlobalLayout>
   )
 }
 
