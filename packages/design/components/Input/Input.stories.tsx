42afccef56129700d66b7b289b62a038ad3d45c1
cokemine <aptx4561@gmail.com>
2022-04-22 19:40:03 +0800

    feat: input component

---
+++ packages/design/components/Input/Input.stories.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { ComponentMeta, Story } from '@storybook/react'
+import Input, { InputProps } from '.'
+import { UserLogin } from '@bangumi/icons'
+
+const componentMeta: ComponentMeta<typeof Input> = {
+  title: 'modern/Input',
+  component: Input
+}
+
+export default componentMeta
+
+// eslint-disable-next-line react/prop-types
+const Template: Story<InputProps> = ({ placeholder, type, prefix, suffix }) => {
+  return (
+    <Input placeholder={placeholder} type={type} prefix={prefix} suffix={suffix} />
+  )
+}
+
+export const Default = Template.bind({})
+
+export const Login = Template.bind({})
+
+Login.args = {
+  placeholder: '你的 Email 地址',
+  prefix: <UserLogin />
+}
+
+export const Password = Template.bind({})
+
+Password.args = {
+  placeholder: '您的密码',
+  type: 'password'
+}
+
+export const Search = Template.bind({})
---
+++ packages/design/components/Input/index.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from 'react'
+import classnames from 'classnames'
+
+export interface InputProps {
+  /* 同原生 `type` */
+  type?: HTMLInputElement['type']
+  /* 同原生 `placeholder` */
+  placeholder?: string
+  /* 外层 wrapper 的样式 */
+  wrapperStyle?: React.CSSProperties
+  /* 外层 wrapper 的自定义类名 */
+  wrapperClass?: string
+  /* 前缀 */
+  prefix?: React.ReactNode
+  /* 后缀 */
+  suffix?: React.ReactNode
+}
+
+const Input: FC<InputProps> = ({
+  type,
+  wrapperStyle,
+  wrapperClass,
+  prefix,
+  suffix,
+  placeholder
+}) => {
+  return (
+    <div className={classnames('bgm-input__wrapper', wrapperClass)} style={wrapperStyle}>
+      {prefix && prefix}
+      <input type={type} className="bgm-input__text" placeholder={placeholder} />
+      {suffix && suffix}
+    </div>
+  )
+}
+
+export default Input
