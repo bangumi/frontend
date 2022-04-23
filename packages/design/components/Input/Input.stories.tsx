4d3ab82086c0a71bc207440a665c239a61f0955c
cokemine <aptx4561@gmail.com>
2022-04-23 16:11:03 +0800

    refactor: style

--- packages/design/components/Input/Input.stories.tsx
+++ packages/design/components/Input/Input.stories.tsx
@@ -11,27 +11,41 @@ const componentMeta: ComponentMeta<typeof Input> = {
 export default componentMeta
 
 // eslint-disable-next-line react/prop-types
-const Template: Story<InputProps> = ({ placeholder, type, prefix, suffix }) => {
+const Template: Story<InputProps> = ({ placeholder, type, prefix, suffix, wrapperClass, wrapperStyle }) => {
   return (
-    <Input placeholder={placeholder} type={type} prefix={prefix} suffix={suffix} />
+    <Input
+      placeholder={placeholder}
+      type={type}
+      prefix={prefix}
+      suffix={suffix}
+      wrapperClass={wrapperClass}
+      wrapperStyle={wrapperStyle}
+    />
   )
 }
 
 export const Default = Template.bind({})
+
+const loginStyle = {
+  width: 320,
+  height: 38
+}
 
 export const Login = Template.bind({})
 
 Login.args = {
   placeholder: '你的 Email 地址',
   type: 'email',
-  prefix: <UserLogin />
+  prefix: <UserLogin style={{ marginRight: 12.5 }} />,
+  wrapperStyle: loginStyle
 }
 
 export const Password = Template.bind({})
 
 Password.args = {
   placeholder: '您的密码',
-  type: 'password'
+  type: 'password',
+  wrapperStyle: loginStyle
 }
 
 export const Search = Template.bind({})
