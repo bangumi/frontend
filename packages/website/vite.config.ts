3f5f2dc1fec0d083b100d63bcde15e7d3ca1b62a
Qingyu Deng <i@ayase-lab.com>
2022-04-28 00:09:58 +0800

    fix: ignore test

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -11,7 +11,13 @@ export default defineConfig({
     pages({
       extensions: ['tsx'],
       importMode: 'async',
-      exclude: ['**/components/**/*.tsx', '*.spec.ts', '*.spec.tsx', '*.test.ts', '*.test.tsx']
+      exclude: [
+        '**/components/**/*.tsx',
+        '**/*.spec.ts',
+        '**/*.spec.tsx',
+        '**/*.test.ts',
+        '**/*.test.tsx'
+      ]
     }),
     styleImport({
       libs: [
