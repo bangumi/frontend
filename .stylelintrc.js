ced872baeb7dc171558a82ed9a560b0136e90abf
神楽坂みずき <45122329+cokemine@users.noreply.github.com>
2022-05-01 00:07:59 +0800

    chore: add stylelint to lint css/less file (#69)
    * feat: stylelint
    
    chore
    
    * modify some rules and fix

---
+++ .stylelintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-css-modules'
+  ],
+  customSyntax: 'postcss-less',
+  rules: {
+    'declaration-colon-newline-after': null,
+    'value-list-comma-newline-after': null,
+    'selector-class-pattern': null,
+    'no-descending-specificity': null,
+    'color-function-notation': 'legacy',
+    'alpha-value-notation': 'number',
+    'string-quotes': 'single'
+  }
+}
