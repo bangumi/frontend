e1136171214294bc853abc286ec4164adca2b7f8
Qingyu Deng <i@ayase-lab.com>
2022-05-06 00:04:48 +0800

    WIP

--- packages/website/src/pages/character/[id]/components/RelatedSubject/index.tsx
+++ packages/website/src/pages/character/[id]/components/RelatedSubject/index.tsx
@@ -1,15 +1,23 @@
-import { Image } from '@bangumi/design'
+import { Image, Typography } from '@bangumi/design'
 import React from 'react'
 import { RelatedSubject as RelatedSubjectType } from '../../../../../types/common'
 import style from './index.module.less'
+import { Link } from 'react-router-dom'
+
+const { Text } = Typography
 
 export interface RelatedSubjectProps {
   subject: RelatedSubjectType
 }
 const RelatedSubject: React.FC<RelatedSubjectProps> = ({ subject }) => {
   return (
-    <div>
+    <div className={style.container}>
       <Image className={style.thumbnail} src={subject.image} />
+      <div className={style.infoLines}>
+        <Link className={style.title} to={`/subject/${subject.id}`}>{subject.name}</Link>
+        <div className={style.translationLine}><Text className={style.translation} type="secondary">{subject.nameCn}</Text></div>
+        <div><span className={style.tag}>{subject.staff}</span></div>
+      </div>
     </div>
   )
 }
