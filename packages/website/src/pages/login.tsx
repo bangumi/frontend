fc024d73e6d61732ce5da604e98816b8d5a010cb
Qingyu Deng <i@ayase-lab.com>
2022-05-09 22:32:46 +0800

    feat: introduce hcaptcha

--- packages/website/src/pages/login.tsx
+++ packages/website/src/pages/login.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import { Input, Button } from '@bangumi/design'
+import { Input } from '@bangumi/design'
 import { UserLogin, Password } from '@bangumi/icons'
+import HCaptcha from '@hcaptcha/react-hcaptcha'
 
 const Login: React.FC = () => {
+  const [, setHCaptchaToken] = React.useState<string | null>(null)
+
   return (
     <div>
-      <Input type="email" prefix={<UserLogin />} placeholder="你的 Email 地址" />
-      <Input type="password" prefix={<Password />} placeholder="你的登录密码" />
-
-      <Button>登录</Button>
+      <form>
+        <Input type="email" prefix={<UserLogin />} placeholder="你的 Email 地址" />
+        <Input type="password" prefix={<Password />} placeholder="你的登录密码" />
+        <HCaptcha
+          sitekey={import.meta.env.VITE_HCAPTCHA_SITE_KEY}
+          onVerify={(token: string) => setHCaptchaToken(token)}
+        />
+      </form>
     </div>
   )
 }
--- packages/website/src/vite-env.d.ts
+++ packages/website/src/vite-env.d.ts
@@ -1,2 +1,6 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pages/client-react" />
+
+interface ImportMetaEnv {
+  readonly VITE_HCAPTCHA_SITE_KEY: string
+}
