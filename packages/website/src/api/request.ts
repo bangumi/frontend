aed0aad6e4c319d63bcc06b7a569cec1b649886b
Qingyu Deng <i@ayase-lab.com>
2022-06-08 10:46:51 +0800

    refactor(website): remove camel case conversion (#89)
    refactor: remove camel case conversion

--- packages/website/src/api/request.ts
+++ packages/website/src/api/request.ts
@@ -14,35 +14,11 @@ const errorHandler = async (error: any): Promise<never> => {
   return await Promise.reject(error)
 }
 
-const underscoreToCamelcase = (data: any): string | any[] => {
-  if (Array.isArray(data)) {
-    data.forEach((value) => {
-      value = underscoreToCamelcase(value)
-    })
-  } else if (data !== null && typeof data === 'object') {
-    for (const key in data) {
-      const newKey = underscoreToCamelcase(key) as string
-      if (key !== newKey) {
-        data[newKey] = data[key]
-        delete data?.key
-      }
-      if (typeof data[key] === 'object') data[key] = underscoreToCamelcase(data[key])
-    }
-  } else if (typeof data === 'string') {
-    data = data.replace(/_[a-z]/g, (str: string): string => {
-      const char = str[1]
-      return char.toUpperCase()
-    })
-  }
-  return data
-}
-
 request.interceptors.request.use(config => {
   return config
 }, errorHandler)
 
 request.interceptors.response.use((response) => {
-  response.data = underscoreToCamelcase(response.data)
   return response
 }, errorHandler)
 
@@ -51,6 +27,5 @@ export const privateRequest = axios.create({
 })
 
 privateRequest.interceptors.response.use((response) => {
-  response.data = underscoreToCamelcase(response.data)
   return response
 })
