481d852427e3e333ca81945caa1be3c3f3037d22
Qingyu Deng <i@ayase-lab.com>
2022-06-08 23:37:28 +0800

    feat: remove home page

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -1,67 +1,8 @@
-import React, { useState, FC, useEffect } from 'react'
-import { Typography } from '@bangumi/design'
-import style from './home.module.css'
-import { getCharacterDetail } from '../api/character'
-import { getSubject } from '../api/subject'
-import { CharacterDetail, Subject } from '../types/common'
+import React from 'react'
 
-const Home: FC = () => {
-  const [detail, setDetail] = useState<CharacterDetail | null>(null)
-  const [subjectDetail, setSubjectDetail] = useState<Subject | null>(null)
-
-  useEffect(() => {
-    getCharacterDetail('39115').then(res => {
-      setDetail(res.data)
-    })
-  }, [])
-
-  useEffect(() => {
-    getSubject('39115').then(res => {
-      setSubjectDetail(res.data)
-    })
-  }, [])
-
+const Home: React.FC = () => {
   return (
-    <>
-      <div className={style.main}>
-        {detail
-          ? <div>
-            <Typography.Link href="https://bgm.tv/character/39115"><h1>{detail.name}</h1></Typography.Link>
-            {
-              detail.infobox.map(el =>
-                <tr key={el.key}>
-                  <td>{el.key}</td>
-                  <tr>
-                    {Array.isArray(el.value)
-                      ? el.value.map(val => <p key={val.k}>{`${val.k ?? ''}:${val.v}`}</p>)
-                      : el.value}
-                  </tr>
-                </tr>
-              )
-            }
-            <p>{detail.summary}</p>
-          </div>
-          : <div>loading</div>}
-        {subjectDetail
-          ? <div>
-            <Typography.Link href="https://bgm.tv/subject/39115"><h1>{subjectDetail.name}</h1></Typography.Link>
-            {
-              subjectDetail.infobox.map(el =>
-                <tr key={el.key}>
-                  <td>{el.key}</td>
-                  <tr>
-                    {Array.isArray(el.value)
-                      ? el.value.map(val => <p key={val.v}>{`${val.v}`}</p>)
-                      : el.value}
-                  </tr>
-                </tr>
-              )
-            }
-            <p>{subjectDetail.summary}</p>
-          </div>
-          : <div>loading</div>}
-      </div>
-    </>
+    null
   )
 }
 
