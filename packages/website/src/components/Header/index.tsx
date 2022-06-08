b08f29656911f8a771a7e516da468bb0045d8f14
shadowdreamer <q781178419@hotmail.com>
2022-06-08 17:20:58 +0800

    fix: issue 90 登录按钮hover

--- packages/website/src/components/Header/index.tsx
+++ packages/website/src/components/Header/index.tsx
@@ -67,51 +67,62 @@ const Header: FC = () => {
   return (
     <div className={style.container}>
       <div className={style.main}>
-        {/* Logo */}
-        <BangumiLogo className={style.logo} />
-        {/* Mobile Logo */}
-        <BangumiTextLogo className={style.logoMobile} />
-        {/* Mobile Menu Toggle Button */}
-        <Button
-          className={style.mobileMenuToggle} shape="rounded" type={showMobileMenu ? 'primary' : 'secondary'}
-          onClick={() => setShowMobileMenu(show => !show)}
-        >
-          {showMobileMenu ? '关闭' : '菜单'}
-        </Button>
-        {/* Menu */}
-        <div className={style.nav}>
-          <Menu
-            items={navLeft} wrapperClass={style.navLeft}
-          />
-          <Divider orientation="vertical" className={style.divider} />
-          <Menu
-            items={navRight} wrapperClass={style.navRight}
-          />
+        <div className="flex items-center">
+          {/* Logo */}
+          <BangumiLogo className={style.logo} />
+          {/* Mobile Logo */}
+          <BangumiTextLogo className={style.logoMobile} />
+          {/* Mobile Menu Toggle Button */}
+          <Button
+            className={style.mobileMenuToggle} shape="rounded" type={showMobileMenu ? 'primary' : 'secondary'}
+            onClick={() => setShowMobileMenu(show => !show)}
+          >
+            {showMobileMenu ? '关闭' : '菜单'}
+          </Button>
+          {/* Menu */}
+          <div className={style.nav}>
+            <Menu
+              items={navLeft} wrapperClass={style.navLeft}
+            />
+            <Divider orientation="vertical" className={style.divider} />
+            <Menu
+              items={navRight} wrapperClass={style.navRight}
+            />
+          </div>
         </div>
-        {/* Search Todo */}
-        <div className={style.infoBox}>
-          <Input
-            prefix={(
-              <>
-                <select name="select" className={style.searchSelect}>
-                  <option value="value1" selected>全部条目</option>
-                  <option value="value2">动画</option>
-                  <option value="value3">书籍</option>
-                  <option value="value4">游戏</option>
-                  <option value="value5">三次元</option>
-                  <option value="value6">人物</option>
-                </select>
-                <Divider orientation="vertical" className={style.searchDivider} />
-              </>
+        <div className="flex items-center">
+          <div className={style.infoBox}>
+            {/* Search Todo */}
+            <Input
+              prefix={(
+                <>
+                  <select name="select" className={style.searchSelect}>
+                    <option value="value1" selected>全部条目</option>
+                    <option value="value2">动画</option>
+                    <option value="value3">书籍</option>
+                    <option value="value4">游戏</option>
+                    <option value="value5">三次元</option>
+                    <option value="value6">人物</option>
+                  </select>
+                  <Divider orientation="vertical" className={style.searchDivider} />
+                </>
             )}
-            suffix={<SearchIcon style={{ flexShrink: 0 }} />}
-            wrapperClass={style.search}
-          />
-          <Notification className={style.iconNotification} />
-          <Setting className={style.iconSetting} />
+              suffix={<SearchIcon style={{ flexShrink: 0 }} />}
+              wrapperClass={style.search}
+            />
+          </div>
+          {/* Avatar */}
+          {user
+            ? <>
+              <Notification className={style.iconNotification} />
+              <Setting className={style.iconSetting} />
+              <Avatar src={user?.avatar?.large} wrapperClass={style.avatar} />
+            </>
+            : <span className={style['user-login']}>
+              <Link className={style.link} to="/login">登录</Link>
+              <Link className={style.link} to="/register">注册</Link>
+            </span>}
         </div>
-        {/* Avatar */}
-        {user ? <Avatar src={user?.avatar?.large} wrapperClass={style.avatar} /> : <Link className={style.link} to="/login">登录</Link>}
       </div>
     </div>
   )
