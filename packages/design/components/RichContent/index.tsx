7933ecf9f4d98bf7e3be6b32baddf082982bfe58
Qingyu Deng <i@ayase-lab.com>
2022-06-22 23:49:59 +0800

    feat: add RichContent

---
+++ packages/design/components/RichContent/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import classNames from 'classnames'
+
+export interface RichTextProps {
+  html: string
+  classname?: string
+}
+
+const RichContent: React.FC<RichTextProps> = ({ html, classname }) => {
+  return (
+    <article
+      className={classNames('bgm-rich-content', classname)}
+      dangerouslySetInnerHTML={{ __html: html }}
+    />
+  )
+}
+
+export default RichContent
