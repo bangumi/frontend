89644aecc5fa87814a3ae42280050e1ec93a7856
Qingyu Deng <i@ayase-lab.com>
2022-07-24 20:42:40 +0800

    feat: add global swr config

--- packages/website/src/main.tsx
+++ packages/website/src/main.tsx
@@ -2,15 +2,24 @@ import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { SWRConfig } from 'swr'
 
 import 'normalize.css'
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <SWRConfig value={{
+      refreshWhenHidden: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+      shouldRetryOnError: false
+    }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SWRConfig>
   </React.StrictMode>,
   document.getElementById('root')
 )
