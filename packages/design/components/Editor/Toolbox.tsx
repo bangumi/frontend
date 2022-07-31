44c10d8b8283539be990e151a18b037fe7bc142a
cokemine <aptx4561@gmail.com>
2022-07-31 14:28:56 +0800

    wip

--- packages/design/components/Editor/Toolbox.tsx
+++ packages/design/components/Editor/Toolbox.tsx
@@ -1,15 +1,19 @@
 import React, { FC } from 'react'
 import { Bold, Image, Italic, Link, Size, Underscore } from '@bangumi/icons'
 
-const Toolbox: FC = () => {
+interface ToolboxProps {
+  handleClickEvent: (type: string, payload?: any) => void
+}
+
+const Toolbox: FC<ToolboxProps> = ({ handleClickEvent }) => {
   return (
     <div className="bgm-editor__header">
-      <Bold />
-      <Italic />
-      <Underscore />
-      <Image />
-      <Link />
-      <Size />
+      <Bold onClick={() => handleClickEvent('bold')} />
+      <Italic onClick={() => handleClickEvent('italic')} />
+      <Underscore onClick={() => handleClickEvent('underscore')} />
+      <Image onClick={() => handleClickEvent('image')} />
+      <Link onClick={() => handleClickEvent('link')} />
+      <Size onClick={() => handleClickEvent('size')} />
     </div>
   )
 }
--- packages/design/components/Editor/index.tsx
+++ packages/design/components/Editor/index.tsx
@@ -1,11 +1,61 @@
-import React, { FC } from 'react'
+/* eslint-disable react/prop-types */
+import React, { FC, ChangeEvent, useRef } from 'react'
 import Toolbox from './Toolbox'
 
-const Editor: FC = () => {
+export interface EditorProps {
+  onChange: (e: ChangeEvent<HTMLTextAreaElement>) => void
+}
+
+const setInputValue = (el: HTMLTextAreaElement, content: string, shouldUpdateSelection = true): void => {
+  const pos = el.value.length + content.lastIndexOf('[')
+  el.value += content
+  shouldUpdateSelection && el.setSelectionRange(pos, pos)
+  el.focus()
+}
+
+const Editor: FC<EditorProps> = ({
+  onChange
+}) => {
+  const ref = useRef<HTMLTextAreaElement>(null)
+  const handleToolboxClick = (type: string, payload?: any): void => {
+    const el = ref.current
+    if (!el) {
+      return
+    }
+    switch (type) {
+      case 'bold': {
+        setInputValue(el, '[b][/b]')
+        break
+      }
+      case 'italic': {
+        setInputValue(el, '[i][/i]')
+        break
+      }
+      case 'underscore': {
+        setInputValue(el, '[u][/u]')
+        break
+      }
+      case 'image': {
+        const value = prompt('请输入图片链接') ?? ''
+        setInputValue(el, `[img]${value}[/img]`, false)
+        break
+      }
+      case 'link': {
+        const value = prompt('请输入链接地址') ?? ''
+        setInputValue(el, `[url=${value}]连接描述[/url]`)
+        break
+      }
+      case 'size': {
+        const value = prompt('请输入字体大小') ?? ''
+        setInputValue(el, `[size=${value}][/size]`)
+        break
+      }
+    }
+  }
   return (
     <div className="bgm-editor__container">
-      <Toolbox />
-      <textarea className="bgm-editor__text" placeholder="想聊点什么的呢..." />
+      <Toolbox handleClickEvent={handleToolboxClick} />
+      <textarea className="bgm-editor__text" placeholder="想聊点什么的呢..." onChange={onChange} ref={ref} />
     </div>
   )
 }
