6240e902e6b490f86e9a991e3bb536bbbe88b295
cokemine <aptx4561@gmail.com>
2022-07-31 22:05:19 +0800

    update tests

--- packages/design/components/EditorForm/__test__/Editor.spec.tsx
+++ packages/design/components/EditorForm/__test__/Editor.spec.tsx
@@ -73,6 +73,65 @@ describe('EditorForm > Editor', () => {
           expect(textarea.selectionEnd).toBe(64)
           break
         }
+        case 'size': {
+          expect(textarea.value).toBe(`[size=${mockValue}][/size]`)
+          expect(textarea.selectionStart).toBe(61)
+          expect(textarea.selectionEnd).toBe(61)
+        }
+      }
+    })
+  })
+
+  it('click toolbox with selection', () => {
+    const ref = React.createRef<HTMLTextAreaElement>()
+    const { getByPlaceholderText } = render(
+      <Editor placeholder="Hello" ref={ref} />
+    )
+    const textarea = getByPlaceholderText('Hello') as HTMLTextAreaElement
+    const mockValue = 'https://lain.bgm.tv/pic/cover/l/65/19/364450_9lB1T.jpg'
+    jest.spyOn(window, 'prompt').mockImplementation(() => mockValue)
+    ;['bold', 'italic', 'underscore', 'image', 'link', 'size'].forEach(type => {
+      ref.current!.value = 'Hello World'
+      textarea.selectionStart = 6
+      textarea.selectionEnd = 11
+      const el = screen.getByTestId(type)
+      el.click()
+      switch (type) {
+        case 'bold': {
+          expect(textarea.value).toBe('Hello [b]World[/b]')
+          expect(textarea.selectionStart).toBe(18)
+          expect(textarea.selectionEnd).toBe(18)
+          break
+        }
+        case 'italic': {
+          expect(textarea.value).toBe('Hello [i]World[/i]')
+          expect(textarea.selectionStart).toBe(18)
+          expect(textarea.selectionEnd).toBe(18)
+          break
+        }
+        case 'underscore': {
+          expect(textarea.value).toBe('Hello [u]World[/u]')
+          expect(textarea.selectionStart).toBe(18)
+          expect(textarea.selectionEnd).toBe(18)
+          break
+        }
+        case 'image': {
+          expect(textarea.value).toBe(`Hello [img]${mockValue}[/img]`)
+          expect(textarea.selectionStart).toBe(71)
+          expect(textarea.selectionEnd).toBe(71)
+          break
+        }
+        case 'link': {
+          expect(textarea.value).toBe(`Hello [url=${mockValue}]World[/url]`)
+          expect(textarea.selectionStart).toBe(77)
+          expect(textarea.selectionEnd).toBe(77)
+          break
+        }
+        case 'size': {
+          expect(textarea.value).toBe(`Hello [size=${mockValue}]World[/size]`)
+          expect(textarea.selectionStart).toBe(79)
+          expect(textarea.selectionEnd).toBe(79)
+        }
       }
     })
   })
