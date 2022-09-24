e5122d418480154018690103ac7987e4dd8903cc
cokemine <aptx4561@gmail.com>
2022-09-24 21:48:08 +0800

    refactor

--- packages/website/src/App.tsx
+++ packages/website/src/App.tsx
@@ -1,12 +1,9 @@
 import React, { FC, Suspense } from 'react'
 import { useRoutes, useLocation } from 'react-router-dom'
 import pageRoutes from '~react-pages'
+import { isNoLayoutRoute } from './utils/route'
 import { UserProvider } from './hooks/use-user'
 import GlobalLayout from './components/GlobalLayout'
-
-const noLayoutPaths = new Set([
-  '/login'
-])
 
 const App: FC = () => {
   const location = useLocation()
@@ -14,7 +11,7 @@ const App: FC = () => {
     <UserProvider>
       <Suspense fallback={null}>
         {
-          noLayoutPaths.has(location.pathname)
+          isNoLayoutRoute(location.pathname)
             ? useRoutes(pageRoutes)
             : <GlobalLayout>{useRoutes(pageRoutes)}</GlobalLayout>
         }
---
+++ packages/website/src/utils/route.ts
@@ -0,0 +1,7 @@
+const noLayoutRoutes = new Set([
+  '/login'
+])
+
+export function isNoLayoutRoute (pathname: string): boolean {
+  return noLayoutRoutes.has(pathname)
+}
