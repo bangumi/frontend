1143e7cfdc5e2b46e40850e4efd2de68f31aaa3c
cokemine <aptx4561@gmail.com>
2022-09-25 20:09:25 +0800

    refactor: remove unused api

--- packages/website/src/api/subject.ts
+++
@@ -1,9 +0,0 @@
-import { request } from './request'
-import { Subject } from '../types/common'
-import { AxiosResponse } from 'axios'
-
-type SubjectId = string
-
-export const getSubject = (subjectId: SubjectId): Promise<AxiosResponse<Subject>> => {
-  return request(`/v0/subjects/${subjectId}`)
-}
