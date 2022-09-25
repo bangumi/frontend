c8c68469f412319de446db93f801a38bb1dbba3f
cokemine <aptx4561@gmail.com>
2022-09-25 20:16:50 +0800

    fix tests

--- packages/types/user.ts
+++ packages/types/user.ts
@@ -3,4 +3,15 @@ import type { components } from './types'
 export * from './common'
 
 export type User = components['schemas']['User']
-export type UserGroup = User['user_group']
+
+export enum UserGroup {
+  Admin = 1,
+  BangumiAdmin = 2,
+  DoujinAdmin = 3,
+  MutedUser = 4,
+  AccessForbiddenUser = 5,
+  CharacterAdmin = 8,
+  WikiAdmin = 9,
+  User = 10,
+  WikiContributor = 11
+}
