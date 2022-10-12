b09f5c2e94609952e63df35cef9c644d11d8d49a
courtier <965405501@qq.com>
2022-10-13 01:17:32 +0800

    fix: no icon group show

--- packages/website/src/pages/group/[name]/components/GroupHeader.tsx
+++ packages/website/src/pages/group/[name]/components/GroupHeader.tsx
@@ -5,10 +5,19 @@ import { Group } from '../../../../types/common'
 import styles from './GroupHeader.module.less'
 
 const { Text } = Typography
+
+function isPic (pic: string): boolean {
+  return /\.(png|jpg|gif|jpeg|webp)/g.test(pic)
+}
+
 export const GroupHeader: React.FC<{group: Group}> = ({ group }) => {
   return (
     <div className={styles.groupHeader}>
-      <Image className={styles.thumbnail} src={group.icon} alt={`${group.title} 头像`} />
+      <div className={styles.thumbnail}>
+        {
+          isPic(group.icon) ? <Image className={styles.thumbnail} src={group.icon} alt={`${group.title} 头像`} /> : null
+        }
+      </div>
       <div className={styles.infoCol}>
         <div className={styles.title}>{group.title}</div>
         <div><Text type="secondary"> 创建于 {dayjs(group.created_at).format('YYYY-M-D HH:mm')} | 现有 {group.total_members} 名成员 </Text></div>
