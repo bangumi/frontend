af8ea68bd954709ce14349d8965ac04d9be0e852
FoundTheWOUT <waua@outlook.com>
2022-10-24 11:54:52 +0800

    fix: type

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -2,7 +2,7 @@ export { default as parseWiki } from './wiki/parser'
 export { render, convert } from './bbcode'
 export * from './bbcode'
 
-export const keyBy = <T extends Record<string, any>, R extends keyof T>(collection: T[], key: R): Record<T[R], T> => {
+export const keyBy = <T extends Record<R, T[R]>, R extends keyof T>(collection: T[], key: R): Record<T[R], T> => {
   return collection.reduce((pre, cur) => ({
     ...pre,
     [cur[key]]: cur
