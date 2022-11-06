abd71006fd563a0905c047437b750310ec1133c0
FoundTheWOUT <waua@outlook.com>
2022-11-06 16:24:22 +0800

    fix: type err

--- packages/website/src/pages/login/index.tsx
+++ packages/website/src/pages/login/index.tsx
@@ -26,6 +26,7 @@ const Login: React.FC = () => {
     [LoginErrorCode.E_SERVER_ERROR]: '服务器错误，请稍后重试'
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
   const handleLogin: () => void = async () => {
     if (!hCaptchaToken) {
       setErrorMessage('请完成验证')
