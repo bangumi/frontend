39c38e8581080b2b8dfd682ceda54994957842dd
Trim21 <trim21.me@gmail.com>
2022-11-14 22:56:26 +0800

    add timeout

--- packages/website/src/api/request.ts
+++ packages/website/src/api/request.ts
@@ -3,7 +3,8 @@ import { Input, Options } from 'ky/distribution/types/options'
 
 export const client = ky.create({
   prefixUrl: import.meta.env.VITE_PRIVATE_API_ROOT,
-  credentials: 'include'
+  credentials: 'include',
+  timeout: 6000
 })
 
 export async function privateGet (url: Input, options?: Options): Promise<any> {
