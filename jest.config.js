018ad0bee13d3a6fa5159aa8a8f2f74d1600ed59
Trim21 <trim21.me@gmail.com>
2022-11-17 01:51:51 +0800

    diff

--- jest.config.js
+++ jest.config.js
@@ -184,6 +184,8 @@ module.exports = {
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
+  //   "/node_modules/",
+  //   "\\.pnp\\.[^\\/]+$"
   // ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
