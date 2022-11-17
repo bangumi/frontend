1104244ce7774f345bf50d5c85099b65c2f54ecd
Trim21 <trim21.me@gmail.com>
2022-11-18 02:52:04 +0800

    update

---
+++ .github/scripts/upload-preview.cjs
@@ -0,0 +1,76 @@
+const fsp = require('node:fs/promises');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const { exec } = require('@actions/exec');
+const github = require('@actions/github');
+const { context } = require('@actions/github');
+
+const artifacts = {
+  Build: 'sites',
+  'Storybook Build': 'storybook',
+};
+
+async function main () {
+  const githubToken = process.env.GH_TOKEN;
+  if (!githubToken) {
+    throw new Error('process.env.GH_TOKEN is empty');
+  }
+  const octokit = github.getOctokit(githubToken);
+
+  const workflow_name = process.env.workflow_name;
+  if (!workflow_name) {
+    throw new Error('process.env.workflow_name is empty');
+  }
+
+  if (Object.keys(artifacts).includes(workflow_name)) {
+    throw new Error(`not valid workflow name ${workflow_name}`);
+  }
+
+  const artifact = artifacts[workflow_name];
+
+  await exec(
+    'gh',
+    [
+      '--repo',
+      `${github.context.repo.owner}/${github.context.repo.repo}`,
+      'run',
+      'download',
+      `${process.env.RUN_ID}`,
+      '--name',
+      artifact,
+      '--dir',
+      artifact,
+    ],
+    {
+      env: {
+        GH_TOKEN: process.env.GH_TOKEN,
+      },
+    },
+  );
+
+  const prNumber = parseInt(
+    fs.readFileSync(path.resolve(artifact, 'pr_number')).toString().trim(),
+  );
+
+  const alias = `pr-${prNumber}-${artifact}`;
+
+  await exec('netlify', ['deploy', `--dir=${artifact}`, `--alias="${alias}"`], {
+    env: {
+      NETLIFY_AUTH_TOKEN: process.env.NETLIFY_AUTH_TOKEN,
+      NETLIFY_SITE_ID: process.env.NETLIFY_SITE_ID,
+    },
+  });
+
+  const comments = await octokit.rest.issues.getComment({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: prNumber,
+  });
+
+  console.log(comments);
+}
+
+main().catch((e) => {
+  throw e;
+});
