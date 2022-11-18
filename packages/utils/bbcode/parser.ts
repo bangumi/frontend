5396cf30baafef8d3db3338b526cea17b108fd4a
Trim21 <trim21.me@gmail.com>
2022-11-19 04:30:28 +0800

    lint

--- packages/utils/bbcode/parser.ts
+++ packages/utils/bbcode/parser.ts
@@ -304,7 +304,7 @@ export class Parser {
   private consumeWhile(checkFn: CheckCharFn): string {
     let result = '';
     while (!this.eof() && checkFn(this.curChar())) {
-      result += this.consumeChar() as string;
+      result += this.consumeChar()!;
     }
     return result;
   }
