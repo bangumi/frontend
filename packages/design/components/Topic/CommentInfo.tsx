233653e4e5658f6d1d7e24c0cb2dbaecb985e213
cokemine <aptx4561@gmail.com>
2022-11-21 16:12:59 +0800

    fix and add tests

--- packages/design/components/Topic/CommentInfo.tsx
+++ packages/design/components/Topic/CommentInfo.tsx
@@ -10,7 +10,7 @@ export interface CommentInfoProps {
 const spaces = '\u00A0'.repeat(2);
 
 const CommentInfo: React.FC<CommentInfoProps> = ({ floor, createdAt, isSpecial = false }) => {
-  const date = dayjs(createdAt).format('YYYY-M-D HH:MM');
+  const date = dayjs(createdAt).format('YYYY-M-D HH:mm');
   return !isSpecial ? (
     <span className='bgm-topic__commentInfo'>
       #{floor}
