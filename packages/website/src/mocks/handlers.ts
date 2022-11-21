250b1ce2277870faf6b7bfceeae128bb8294bab7
Trim21 <trim21.me@gmail.com>
2022-11-21 23:39:12 +0800

    stage

--- packages/website/src/mocks/handlers.ts
+++ packages/website/src/mocks/handlers.ts
@@ -1,15 +1,3 @@
 import { mockAPI } from './utils';
 
-const BASE_URL = (import.meta.env.VITE_APP_ROOT as string) ?? 'https://api.bgm.tv';
-
-function buildAPIURL(path: string): string {
-  return `${BASE_URL}${path}`;
-}
-
-export const handlers = [
-  mockAPI(buildAPIURL('/v0/subjects/:subjectId'), 'get'),
-  mockAPI(buildAPIURL('/v0/persons/:personId/characters'), 'get'),
-  mockAPI(buildAPIURL('/v0/characters/:characterId'), 'get'),
-  mockAPI(buildAPIURL('/v0/me'), 'get'),
-  mockAPI('/p/me', 'get'),
-];
+export const handlers = [mockAPI('/p/me', 'get')];
