3736561f11f32b2d33b9ef43c38ca1d5cdc05a6b
Trim21 <trim21.me@gmail.com>
2022-12-18 02:28:47 +0800

    enable ws

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -29,6 +29,7 @@ export default defineConfig(({ mode }) => {
         '/p': {
           target: apiDomain,
           changeOrigin: true,
+          ws: true,
           configure(proxy) {
             proxy.on('proxyReq', (proxyReq) => {
               if (proxyReq.hasHeader('Origin')) {
