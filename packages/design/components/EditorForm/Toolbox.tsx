742b8a847cbfa2059eb2268d3c2b798d77da6bcf
Googleplex <yyoung2001@gmail.com>
2022-12-26 23:00:47 +0800

    feat: show tip for toolbox icons (#270)

--- packages/design/components/EditorForm/Toolbox.tsx
+++ packages/design/components/EditorForm/Toolbox.tsx
@@ -11,12 +11,20 @@ interface ToolboxProps {
 const Toolbox: FC<ToolboxProps> = ({ handleClickEvent, style }) => {
   return (
     <div className='bgm-editor__toolbox' style={style}>
-      <Bold data-testid='bold' onClick={() => handleClickEvent('bold')} />
-      <Italic data-testid='italic' onClick={() => handleClickEvent('italic')} />
-      <Underscore data-testid='underscore' onClick={() => handleClickEvent('underscore')} />
-      <Image data-testid='image' onClick={() => handleClickEvent('image')} />
-      <Link data-testid='link' onClick={() => handleClickEvent('link')} />
-      <Size data-testid='size' onClick={() => handleClickEvent('size')} />
+      <Bold data-testid='bold' onClick={() => handleClickEvent('bold')} title='粗体 [Ctrl+B]' />
+      <Italic
+        data-testid='italic'
+        onClick={() => handleClickEvent('italic')}
+        title='斜体 [Ctrl+I]'
+      />
+      <Underscore
+        data-testid='underscore'
+        onClick={() => handleClickEvent('underscore')}
+        title='下划线 [Ctrl+U]'
+      />
+      <Image data-testid='image' onClick={() => handleClickEvent('image')} title='图片 [Ctrl+P]' />
+      <Link data-testid='link' onClick={() => handleClickEvent('link')} title='链接 [Ctrl+L]' />
+      <Size data-testid='size' onClick={() => handleClickEvent('size')} title='文字尺寸 [Ctrl+S]' />
     </div>
   );
 };
