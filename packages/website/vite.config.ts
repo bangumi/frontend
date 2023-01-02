851ced5357db64978cca69e62dde348c45d2efd8
Trim21 <trim21me@gmail.com>
2023-01-02 10:31:31 +0800

    chore: use stage api as default backend (#294)

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -6,12 +6,13 @@ import pages from 'vite-plugin-pages';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig(({ mode }) => {
-  const apiDomain =
-    mode === 'loc'
-      ? 'http://127.0.0.1:4000'
-      : ['stage', 'development'].includes(mode)
-      ? 'https://dev.bgm38.com'
-      : 'https://next.bgm.tv';
+  let apiDomain = 'https://dev.bgm38.com';
+
+  if (mode === 'loc') {
+    apiDomain = 'http://127.0.0.1:4000';
+  } else if (mode === 'production') {
+    apiDomain = 'https://next.bgm.tv';
+  }
 
   console.log('using backend', apiDomain);
 
