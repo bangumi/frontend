4ba272a291128944d1fd1a1124e2d87fcc26e03b
Trim21 <trim21.me@gmail.com>
2023-01-11 13:30:36 +0800

    use sse

--- packages/website/src/hooks/use-notify.ts
+++ packages/website/src/hooks/use-notify.ts
@@ -1,12 +1,9 @@
 import { useEffect, useState } from 'react';
-import type { Socket } from 'socket.io-client';
-import { io } from 'socket.io-client';
 
 import type { User } from '@bangumi/client/client';
 
 export function useNotify(user: User | undefined) {
   const [noticeCount, setNoticeCount] = useState(0);
-  const [socket, setSocket] = useState<Socket | null>(null);
 
   useEffect(() => {
     if (!user) {
@@ -14,22 +11,16 @@ export function useNotify(user: User | undefined) {
     }
 
     console.log('new websocket');
-    const ws = io(location.host, {
-      path: '/p1/socket-io/',
-      reconnection: true,
-      reconnectionDelay: 5000,
-      reconnectionDelayMax: 10000,
-    });
+    const source = new EventSource('/p1/sse/notify');
 
-    ws.on('notify', (event: string) => {
-      const { count } = JSON.parse(event) as { count: number };
+    source.addEventListener('notify-change', ({ data }: { data: string }) => {
+      const { count } = JSON.parse(data) as { count: number };
       setNoticeCount(count);
     });
 
-    setSocket(ws);
     return () => {
       console.log('close connection');
-      ws.disconnect();
+      source.close();
     };
   }, [user]);
 
