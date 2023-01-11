b1af756c3dfea1809af4815a7e1e7e1ec988f106
Trim21 <trim21me@gmail.com>
2023-01-11 14:54:53 +0800

    refactor: use eventemitter3 generic type paramaters (#309)

--- packages/design/components/Toast/utils/event-bus.ts
+++ packages/design/components/Toast/utils/event-bus.ts
@@ -2,32 +2,33 @@ import EventEmitter from 'eventemitter3';
 
 import type { Toast } from '../types';
 
-const toastEventBus = new EventEmitter();
-
 enum ToastEvent {
   InsertToast = 'INSERT_TOAST',
   RemoveToast = 'REMOVE_TOAST',
 }
 
-const createEventOperators = <T extends (...param: any[]) => void>(eventName: string) => {
+interface EventTypes {
+  [ToastEvent.InsertToast]: (newToast: Toast) => void;
+  [ToastEvent.RemoveToast]: (toast: Toast) => void;
+}
+
+const toastEventBus = new EventEmitter<EventTypes>();
+
+const createEventOperators = <T extends ToastEvent>(eventName: T) => {
   return {
-    subscribe: (handler: T) => {
+    subscribe: (handler: EventTypes[T]) => {
       toastEventBus.on(eventName, handler);
     },
-    unsubscribe: (handler: T) => {
+    unsubscribe: (handler: EventTypes[T]) => {
       toastEventBus.off(eventName, handler);
     },
-    emit(...params: Parameters<T>) {
+    emit(...params: Parameters<EventTypes[T]>) {
       toastEventBus.emit(eventName, ...params);
     },
   };
 };
 
 /* eslint-disable @typescript-eslint/unbound-method */
-export const insertToastEvent = createEventOperators<(newToast: Toast) => void>(
-  ToastEvent.InsertToast,
-);
+export const insertToastEvent = createEventOperators(ToastEvent.InsertToast);
 
-export const removeToastEvent = createEventOperators<(toast: Toast) => void>(
-  ToastEvent.RemoveToast,
-);
+export const removeToastEvent = createEventOperators(ToastEvent.RemoveToast);
