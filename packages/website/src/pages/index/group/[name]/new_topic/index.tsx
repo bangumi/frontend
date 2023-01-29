502b8f6aa19fbf5ff176280454cfd6b612ce0d60
yyoung <yyoung2001@gmail.com>
2023-01-29 15:45:46 +0800

    feat: create new group topic (finished)

--- packages/website/src/pages/index/group/[name]/new_topic/index.tsx
+++ packages/website/src/pages/index/group/[name]/new_topic/index.tsx
@@ -1,17 +1,38 @@
 import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import type { SubmitHandler } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
+import { useNavigate, useParams } from 'react-router-dom';
 
+import { ozaClient } from '@bangumi/client';
 import { EditorForm, Input, Typography } from '@bangumi/design';
 import { useGroup } from '@bangumi/website/hooks/use-group';
 
 import GroupInfo from '../../components/GroupInfo';
 import styles from './style.module.less';
 
+interface FormData {
+  title: string;
+  content: string;
+}
+
 const GroupNewTopicPage = () => {
+  const navigate = useNavigate();
   const { name } = useParams();
   const { group } = useGroup(name!);
 
-  const [content, setContent] = useState('');
+  const { register, handleSubmit, control } = useForm<FormData>();
+  const [sending, setSending] = useState(false);
+
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    setSending(true);
+    const response = await ozaClient.createNewGroupTopic(group.group.name, data);
+    if (response.status === 200) {
+      navigate(`/group/topic/${response.data.id}`);
+    } else {
+      console.error(response); // TODO: display error
+    }
+    setSending(false);
+  };
 
   return (
     <>
@@ -27,16 +48,28 @@ const GroupNewTopicPage = () => {
             </Typography.Text>
           </div>
           <div className={styles.titleInput}>
-            <Input placeholder='取个标题……' />
+            <Input placeholder='取个标题……' {...register('title', { required: true })} />
           </div>
         </div>
         <div className={styles.contentEditor}>
-          <EditorForm
-            placeholder='话题正文……'
-            content={content}
-            onChange={setContent}
-            hideCancel
-            rows={15}
+          <Controller
+            name='content'
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <EditorForm
+                placeholder='话题正文……'
+                onConfirm={() => {
+                  // TODO: display validation error
+                  handleSubmit(onSubmit, (errors) => console.log(errors))();
+                }}
+                hideCancel
+                // TODO: use loading state
+                confirmText={sending ? '...' : undefined}
+                rows={15}
+                {...field}
+              />
+            )}
           />
         </div>
         <div className={styles.rightColumn}>
