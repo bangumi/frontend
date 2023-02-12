617fa1238f14f0aa1fcbea1c4b559b7932ef0a69
yyoung <yyoung2001@gmail.com>
2023-02-12 22:12:21 +0800

    refactor: align GroupNavigation with design

--- packages/website/src/pages/index/group/components/GroupNavigation.tsx
+++ packages/website/src/pages/index/group/components/GroupNavigation.tsx
@@ -1,7 +1,7 @@
 import type { GroupProfile } from 'packages/client/group';
 import React from 'react';
 
-import { Button, Typography } from '@bangumi/design';
+import { Button, Section, Typography } from '@bangumi/design';
 
 import styles from './GroupNavigation.module.less';
 
@@ -9,25 +9,28 @@ const Link = Typography.Link;
 
 const GroupNavigation = ({ group }: { group: GroupProfile }) => {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.section}>
-        <Button.Link color='blue' to={`/group/${group.group.name}/new_topic`}>
-          发表新主题
-        </Button.Link>
+    <Section title='小组功能'>
+      <div className={styles.wrapper}>
+        <div className={styles.section}>
+          <Button.Link color='blue' to={`/group/${group.group.name}/new_topic`}>
+            发表新主题
+          </Button.Link>
+          <Button color='blue' type='secondary'>
+            {group.inGroup ? '退出' : '加入'}该小组
+          </Button>
+        </div>
         <div className={styles.links}>
-          话题：<Link to='/group/my_topic'>我发表的</Link> |{' '}
-          <Link to='/group/my_reply'>我回复的</Link>
+          <div>
+            话题：<Link to='/group/my_topic'>我发表的</Link> |{' '}
+            <Link to='/group/my_reply'>我回复的</Link>
+            <br />
+          </div>
+          <div>
+            小组：<Link to='#'>我管理的</Link> | <Link to='/group/mine'>我参加的</Link>
+          </div>
         </div>
       </div>
-      <div className={styles.section}>
-        <Button color='blue' type='secondary'>
-          {group.inGroup ? '退出' : '加入'}这个小组
-        </Button>
-        <div className={styles.links}>
-          小组：<Link to='#'>我管理的</Link> | <Link to='/group/mine'>我参加的</Link>
-        </div>
-      </div>
-    </div>
+    </Section>
   );
 };
 
