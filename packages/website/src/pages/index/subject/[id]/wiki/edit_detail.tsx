b461e160567fee06a344082754740fd4d466147b
Qingyu Deng <i@ayase-lab.com>
2023-02-13 01:30:51 +0800

    feat(website): add line number in toast when error occurred (#321)
    Co-authored-by: Trim21 <trim21me@gmail.com>

--- packages/website/src/pages/index/subject/[id]/wiki/edit_detail.tsx
+++ packages/website/src/pages/index/subject/[id]/wiki/edit_detail.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-useless-return */
 import cn from 'classnames';
 import dayjs from 'dayjs';
 import { cloneDeep, concat, filter, flow, isArray, isNumber, set } from 'lodash';
@@ -228,6 +227,10 @@ interface FormData {
   subject: ozaClient.SubjectEdit;
 }
 
+const formatWikiSyntaxErrorMessage = (error: WikiSyntaxError): string => {
+  return `${error.lino ? `line ${error.lino}:` : ''} ${error.message}`;
+};
+
 const WikiEditDetailDetailPage: React.FC = () => {
   const { register, handleSubmit, setValue, watch } = useForm<FormData>();
   const prePlatform = watch('subject.platform');
@@ -280,7 +283,7 @@ const WikiEditDetailDetailPage: React.FC = () => {
         }
       } catch (error: unknown) {
         if (error instanceof WikiSyntaxError) {
-          toast(error.message);
+          toast(formatWikiSyntaxErrorMessage(error));
         }
         return;
       }
@@ -330,7 +333,7 @@ const WikiEditDetailDetailPage: React.FC = () => {
         } catch (error: unknown) {
           setEditorType(EditorType.Wiki);
           if (error instanceof WikiSyntaxError) {
-            toast(error.message);
+            toast(formatWikiSyntaxErrorMessage(error));
           }
         }
         break;
