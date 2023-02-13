86e50717605c79a436fe6052c9c43262ceb024fc
yyoung <yyoung2001@gmail.com>
2023-02-13 10:22:35 +0800

    feat: hide word count when no content

--- packages/design/components/EditorForm/Editor.tsx
+++ packages/design/components/EditorForm/Editor.tsx
@@ -195,7 +195,9 @@ const Editor = forwardRef<HTMLTextAreaElement, EditorProps>(
       >
         <div className='bgm-editor__toolbar'>
           {showToolbox && <Toolbox handleClickEvent={handleToolboxEvent} />}
-          {showWordCount && <div className='bgm-editor__wordcount'>已输入 {size(value)} 字</div>}
+          {showWordCount && value && (
+            <div className='bgm-editor__wordcount'>已输入 {size(value)} 字</div>
+          )}
         </div>
         <textarea
           className={classnames('bgm-editor__text', { 'bgm-editor__text--width-auto': inForm })}
