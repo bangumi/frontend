600cd5187957265350215455e848fd1c75af2e25
Trim21 <trim21.me@gmail.com>
2023-02-19 15:21:37 +0800

    comment

--- test-setup.ts
+++ test-setup.ts
@@ -1,10 +1,8 @@
 import matchers from '@testing-library/jest-dom/matchers';
 import { cleanup } from '@testing-library/react';
-// import fetch from 'cross-fetch';
 import { afterEach, expect } from 'vitest';
 
 expect.extend(matchers);
-// vi.stubGlobal('fetch', fetch);
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
