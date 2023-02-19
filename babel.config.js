ee80877dcc0c034f0583e3f9049ef05c002c374f
Trim21 <trim21.me@gmail.com>
2023-02-19 15:15:58 +0800

    babel deps

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-// storybook 需要 babel 加载配置文件
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-typescript',
-    '@babel/preset-react',
-    'babel-preset-vite',
-  ],
-};
