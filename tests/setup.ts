cf089312e3e1ff914bc45ad5182dc5879cc99513
Trim21 <trim21.me@gmail.com>
2023-02-20 12:44:11 +0800

    no stub jest

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,10 +1,7 @@
 import matchers from '@testing-library/jest-dom/matchers';
 import { cleanup } from '@testing-library/react';
-import { vi } from 'vitest';
 
 expect.extend(matchers);
-
-vi.stubGlobal('jest', vi);
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
