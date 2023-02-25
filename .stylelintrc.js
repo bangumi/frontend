12b5661e99a682646b79ea84ab72f1950109b282
Trim21 <trim21me@gmail.com>
2023-02-25 16:01:33 +0800

    style: remove deprecated rules (#418)

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,13 +3,10 @@ module.exports = {
   customSyntax: 'postcss-less',
   rules: {
     'import-notation': null,
-    'declaration-colon-newline-after': null,
-    'value-list-comma-newline-after': null,
     'selector-class-pattern': null,
     'no-descending-specificity': null,
     'color-function-notation': 'legacy',
     'alpha-value-notation': 'number',
-    'string-quotes': 'single',
     'function-no-unknown': [true, { ignoreFunctions: ['data-uri'] }],
   },
 };
