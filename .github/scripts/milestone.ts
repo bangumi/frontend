752d8beeeea8459d3a410092c3aa789533c29343
Trim21 <trim21.me@gmail.com>
2023-03-19 11:26:17 +0800

    move open issues

--- .github/scripts/milestone.ts
+++ .github/scripts/milestone.ts
@@ -3,11 +3,20 @@ import * as fs from 'node:fs';
 
 import * as github from '@actions/github';
 
+import type { components } from '@octokit/openapi-types';
+
+type Milestone = components['schemas']['nullable-milestone'];
+
 const packageJSON = JSON.parse(
   fs.readFileSync(path.join(__dirname, '../../package.json')).toString(),
 );
 
 const { version } = packageJSON;
+
+const repo = {
+  owner: 'bangumi',
+  repo: 'frontend',
+};
 
 async function main() {
   const githubToken = process.env.GH_TOKEN;
@@ -17,27 +26,56 @@ async function main() {
 
   const octokit = github.getOctokit(githubToken);
 
-  const milestones = await octokit.paginate('GET /repos/{owner}/{repo}/milestones', {
-    owner: 'bangumi',
-    repo: 'frontend',
-  });
+  const milestones = await octokit.paginate('GET /repos/{owner}/{repo}/milestones', { ...repo });
+
+  let oldNextMilestone: undefined | Milestone;
 
   for (const milestone of milestones) {
-    console.log(milestone);
     if (milestone.title === 'next') {
-      await octokit.request('PATCH /repos/{owner}/{repo}/milestones/{milestone_number}', {
-        milestone_number: milestone.number,
-        owner: 'bangumi',
-        repo: 'frontend',
-        title: version,
-      });
+      oldNextMilestone = milestone;
       break;
     }
   }
 
-  await octokit.request('POST /repos/{owner}/{repo}/milestones', {
-    owner: 'bangumi',
-    repo: 'frontend',
+  if (!oldNextMilestone) {
+    return;
+  }
+
+  const openIssues = await octokit.paginate('GET /repos/{owner}/{repo}/issues', {
+    ...repo,
+    state: 'open',
+    milestone: oldNextMilestone.number,
+  });
+
+  await octokit.request('PATCH /repos/{owner}/{repo}/milestones/{milestone_number}', {
+    ...repo,
+    milestone_number: oldNextMilestone.number,
+    title: version,
+  });
+
+  const newNextMileStone = await octokit.request('POST /repos/{owner}/{repo}/milestones', {
+    ...repo,
+    title: 'next-next',
+  });
+
+  for (const issue of openIssues) {
+    await octokit.request('PATCH /repos/{owner}/{repo}/issues/{issue_number}', {
+      ...repo,
+      issue_number: issue.number,
+      milestone: newNextMileStone.data.number,
+    });
+  }
+
+  await octokit.request('PATCH /repos/{owner}/{repo}/milestones/{milestone_number}', {
+    ...repo,
+    milestone_number: oldNextMilestone.number,
+    title: version,
+    state: 'closed',
+  });
+
+  await octokit.request('PATCH /repos/{owner}/{repo}/milestones/{milestone_number}', {
+    ...repo,
+    milestone_number: newNextMileStone.data.number,
     title: 'next',
   });
 }
