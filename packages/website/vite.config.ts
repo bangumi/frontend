bae42dc6482270d1048aeadfbf0f5ed971ba7ce5
Trim21 <trim21.me@gmail.com>
2023-03-28 18:02:51 +0800

    chore: switch stage api backend domain

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -23,7 +23,7 @@ dayjs.extend(utc);
 const BUILD_TIME = dayjs().utc().format();
 
 export default defineConfig(({ mode }) => {
-  let apiDomain = 'https://dev.bgm38.com';
+  let apiDomain = 'https://next.bgm38.com';
 
   if (mode === 'loc') {
     apiDomain = 'http://127.0.0.1:4000';
