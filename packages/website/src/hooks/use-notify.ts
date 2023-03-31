6a02f685c6d3524df911de8ef16116cbd6bab5a5
Waua <waua@outlook.com>
2023-03-31 16:25:41 +0800

    Watch title change in useNotify

--- packages/website/src/hooks/use-notify.ts
+++ packages/website/src/hooks/use-notify.ts
@@ -9,6 +9,17 @@ export function useNotify(user: User | undefined) {
   const [noticeCount, setNoticeCount] = useState(0);
 
   useEffect(() => {
+    // TODO: reset title when count is 0
+    if (noticeCount > 0) {
+      document.title = `(${noticeCount})...${document.title}`;
+    }
+    // 正常来说，document.title 变成不会触发 rerender，这个 effect 也不会重新运行
+    // 但现在 useNotify 只在 Header 使用，而 Header 使用了 react-router 的 hook，当路由变化时
+    // Header 会 rerender，使得该 effect 有重新执行的机会。
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [noticeCount, document.title]);
+
+  useEffect(() => {
     if (!user) {
       return;
     }
@@ -23,8 +34,9 @@ export function useNotify(user: User | undefined) {
 
       socket.on('notify', (event: string) => {
         const { count } = JSON.parse(event) as { count: number };
-        // TODO: reset title when count is 0
-        document.title = `(${count})...${document.title}`;
+        // if (noticeCount) {
+        // document.title = `(${noticeCount})...${document.title}`;
+        // }
         setNoticeCount(count);
       });
     }
