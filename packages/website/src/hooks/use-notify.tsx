7bb1f2af0eac2e4af7735e24019ddbd31851daad
Trim21 <trim21.me@gmail.com>
2023-04-03 23:43:33 +0800

    fix context type

--- packages/website/src/hooks/use-notify.tsx
+++ packages/website/src/hooks/use-notify.tsx
@@ -56,6 +56,6 @@ export const NoticeProvider: React.FC<PropsWithChildren> = ({ children }) => {
   return <NoticeContext.Provider value={value}> {children} </NoticeContext.Provider>;
 };
 
-export const useNotify: () => NoticeContextType = () => {
+export function useNotify(): NoticeContextType | null {
   return React.useContext(NoticeContext);
-};
+}
