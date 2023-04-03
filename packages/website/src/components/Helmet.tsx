6d07f7ea8549541d2279f135e3a1d7e33ad487eb
Trim21 <trim21.me@gmail.com>
2023-04-03 23:17:40 +0800

    use notice provider

--- packages/website/src/components/Helmet.tsx
+++ packages/website/src/components/Helmet.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Helmet as BaseHelmet, type HelmetProps } from 'react-helmet-async';
 
+import { useNotify } from '@bangumi/website/hooks/use-notify.tsx';
+
 const Helmet = (props: React.PropsWithChildren<HelmetProps>) => {
+  const { noticeCount } = useNotify();
+
+  useEffect(() => {
+    console.log(noticeCount);
+  }, [noticeCount]);
+
   return (
-    <BaseHelmet defaultTitle='Bangumi 番组计划' titleTemplate='%s | Bangumi 番组计划' {...props} />
+    <BaseHelmet
+      defaultTitle='Bangumi 番组计划'
+      titleTemplate={
+        noticeCount ? `(${noticeCount}) %s | Bangumi 番组计划` : `%s | Bangumi 番组计划`
+      }
+      {...props}
+    />
   );
 };
 
