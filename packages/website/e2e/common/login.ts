99afa60dcdf4e1cde07b358c272d53f78d773bed
Qingyu Deng <i@ayase-lab.com>
2023-08-09 00:10:11 +0800

    test: fix e2e failure

--- packages/website/e2e/common/login.ts
+++ packages/website/e2e/common/login.ts
@@ -2,7 +2,7 @@ import type { Page } from '@playwright/test';
 import { expect } from '@playwright/test';
 
 const login = async (page: Page) => {
-  await page.getByRole('link', { name: '登录' }).click();
+  await page.goto('/login');
   await page.getByPlaceholder('你的 Email 地址').click();
   await page.getByPlaceholder('你的 Email 地址').fill('treeholechan@gmail.com');
   await page.getByPlaceholder('你的登录密码').click();
