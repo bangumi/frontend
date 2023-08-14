3f18a8fadbca95fcc3ea3493905e72d9531c921b
Trim21 <trim21.me@gmail.com>
2023-08-14 23:03:04 +0800

    update

--- a.mjs
+++
@@ -1,8 +0,0 @@
-import fs from 'node:fs';
-import path from 'node:path';
-
-import fse from 'fs-extra';
-
-const uploadedFunctions = path.resolve('sites', 'functions');
-fs.rmSync(uploadedFunctions, { force: true, recursive: true });
-fse.copySync('packages/website/public/_functions', uploadedFunctions);
