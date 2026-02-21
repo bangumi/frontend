8093f1ad944b9c5ce44aac08b8345745ed8d5c5d
Trim21 <trim21.me@gmail.com>
2026-02-21 19:09:01 +0800

    feat(svg): update SVG imports to use named exports as React components

--- packages/svg.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.svg' {
-  import type { FC, SVGProps } from 'react';
-
-  const src: string;
-  export default src;
-
-  export const ReactComponent: FC<SVGProps<SVGSVGElement>>;
-}
--- packages/website/src/vite-env.d.ts
+++ packages/website/src/vite-env.d.ts
@@ -6,10 +6,17 @@ interface ImportMetaEnv {
 }
 
 declare module '*.svg' {
-  import type { FC, SVGProps } from 'react';
+  import type { ComponentProps,FC } from 'react';
 
   const src: string;
   export default src;
 
-  export const ReactComponent: FC<SVGProps<SVGSVGElement>>;
+  export const ReactComponent: FC<
+    ComponentProps<'svg'> & {
+      title?: string;
+      titleId?: string;
+      desc?: string;
+      descId?: string;
+    }
+  >;
 }
