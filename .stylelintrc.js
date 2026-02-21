64e84948f911ebdab2309391fafa05407c9413d7
Trim21 <trim21.me@gmail.com>
2026-02-21 19:48:33 +0800

    test: align stylelint and vitest setup for upgrades

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -7,6 +7,12 @@ module.exports = {
     'no-descending-specificity': null,
     'color-function-notation': 'legacy',
     'alpha-value-notation': 'number',
+    'at-rule-prelude-no-invalid': null,
+    'media-query-no-invalid': null,
+    'declaration-property-value-no-unknown': null,
+    'color-function-alias-notation': null,
+    'media-feature-range-notation': null,
+    'declaration-block-no-redundant-longhand-properties': null,
     'function-no-unknown': [true, { ignoreFunctions: ['data-uri'] }],
   },
 };
